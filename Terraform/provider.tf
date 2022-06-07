fc77fe0df9c95480baaa7f57e88d139c05d746ed
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-07 10:27:36 +0300

    adding terraform files

---
+++ Terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "market-user-ms-s3-terraform"
+    key    = "terraform/market-user-ms/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
