2b3886ea56e2b7dd90fc404a95c67ebb53b1ad62
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-07 11:42:17 +0300

    Everything as var - terraform

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "project_name" {
   type = string
-  default = "market-user-ms-repository"
 }
 
 variable "project_region" {
     type = string
-    default = "us-east-1"
 }
 
 variable "project_env" {
     type = string
-    default = "dev"
 }
