ea3bb704a47e2147a8c2f73f42328aef7feec7b2
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-08 13:15:52 +0300

    bucket name fix

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
   #variables like ${github.var} are being replaced using sed command in pipeline. 
   #We are not able to parse terraform variables to backend parameters
   backend "s3" { 
-    bucket = "${github.project_name}-s3-terraform"
+    bucket = "${github.bucket_name}"
     key    = "${github.project_name}/terraform/${github.project_env}/terraform.tfstate"
     region = "${github.region}"
   }
