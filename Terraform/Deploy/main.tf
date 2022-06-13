16de4bf4f885f60d2b05f9c717a3bd04dc403303
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-13 17:33:58 +0300

    intra subnets to private subnets

--- Terraform/Deploy/main.tf
+++ Terraform/Deploy/main.tf
@@ -24,7 +24,7 @@ module "eks" {
   cluster_version = "1.21"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.intra_subnets
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     green = {
