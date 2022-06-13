f5fbc63e887528a14e266fbbb874d05b6fb3cec7
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-13 13:18:49 +0300

    terraform changes

--- Terraform/Build/main.tf
+++ Terraform/Build/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "marketplace_ecr" {
-  name                 = "${var.project_name}-repository"
+  name                 = "${var.project_name}-${var.project_env}"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
