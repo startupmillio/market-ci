2c469743c715994a90279479c265d7a571abf662
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-13 15:32:45 +0300

    k8s deploy 1 node test

---
+++ Terraform/Deploy/main.tf
@@ -0,0 +1,38 @@
+locals {
+  cluster_name = "${var.project_name}"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  aws_region = "us-east-1"
+  az_count   = 3
+  aws_azs    = "us-east-1a, us-east-1b, us-east-1c"
+
+  global_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = "${var.project_name}"
+  cluster_version = "1.21"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.intra_subnets
+
+  eks_managed_node_groups = {
+    green = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+
+      instance_types = ["t2.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+}
---
+++ Terraform/Deploy/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  #variables like ${github.var} are being replaced using sed command in pipeline. 
+  #We are not able to parse terraform variables to backend parameters
+  backend "s3" { 
+    bucket = "${github.bucket_name}"
+    key    = "${github.project_name}/terraform/general/terraform.tfstate"
+    region = "${github.region}"
+  }
+}
+
+provider "aws" {
+  region = "${var.project_region}"
+}
