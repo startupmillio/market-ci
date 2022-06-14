c3afc89d6256bdf34ea8095f928f3d7a354b31d5
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-14 20:48:48 +0300

    adding domain as variable to helm

--- Terraform/Deploy/helm.tf
+++ Terraform/Deploy/helm.tf
@@ -8,4 +8,10 @@ resource "helm_release" "contour_ingress_configure" {
   name       = "contour-ingress-controller"
   chart      = "../../Helm/ingress-chart"
   depends_on = [helm_release.contour_ingress_controller]
+
+  set {
+    name = "domain"
+    value = "${var.project_name}.${data.aws_route53_zone.market_zone.name}"
+  }
+
 }
