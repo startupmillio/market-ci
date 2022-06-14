b4943f62482045345bd1cd488457d92ab8b09711
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-14 15:57:33 +0300

    route53 record as alias record

--- Terraform/Deploy/route53.tf
+++ Terraform/Deploy/route53.tf
@@ -8,26 +8,16 @@ data "aws_route53_zone" "market_zone" {
   name         = "${var.route53_zone}"
 }
 
-resource "aws_route53_record" "prod" {
-  zone_id = data.aws_route53_zone.market_zone.zone_id
-  name    = "prod.${var.project_name}.${data.aws_route53_zone.market_zone.name}"
-  type    = "A"
-  ttl     = "300"
-  records = [data.kubernetes_service.contour.status.0.load_balancer.0.ingress.0.hostname]
-}
-
 resource "aws_route53_record" "dev" {
   zone_id = data.aws_route53_zone.market_zone.zone_id
   name    = "dev.${var.project_name}.${data.aws_route53_zone.market_zone.name}"
   type    = "A"
-  ttl     = "300"
-  records = [data.kubernetes_service.contour.status.0.load_balancer.0.ingress.0.hostname]
-}
 
-resource "aws_route53_record" "qa" {
-  zone_id = data.aws_route53_zone.market_zone.zone_id
-  name    = "qa.${var.project_name}.${data.aws_route53_zone.market_zone.name}"
-  type    = "A"
-  ttl     = "300"
-  records = [data.kubernetes_service.contour.status.0.load_balancer.0.ingress.0.hostname]
+  //records = [data.kubernetes_service.contour.status.0.load_balancer.0.ingress.0.hostname]
+
+  alias {
+    name                   = data.kubernetes_service.contour.status.0.load_balancer.0.ingress.0.hostname
+    zone_id                = data.aws_route53_zone.market_zone.zone_id
+    evaluate_target_health = true
+  }
 }
