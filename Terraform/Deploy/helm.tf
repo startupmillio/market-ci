97ed8c8695f6051c27b91720244dc2bb8f41a636
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-14 20:33:24 +0300

    helm ingress config deploy

--- Terraform/Deploy/helm.tf
+++ Terraform/Deploy/helm.tf
@@ -3,3 +3,9 @@ resource "helm_release" "contour_ingress_controller" {
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "contour"
 }
+
+resource "helm_release" "contour_ingress_controller" {
+  name       = "contour-ingress-controller"
+  chart      = "../../Helm/ingress-chart"
+  depends_on = [helm_release.contour_ingress_controller]
+}
