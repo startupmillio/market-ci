2ecbc1b04fbd6bf52ea2e80e1903b9bfb7e1d0a0
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-17 14:29:08 +0300

    fixed project tf vars

--- Terraform/Project/variables.tf
+++ Terraform/Project/variables.tf
@@ -14,22 +14,6 @@ variable "vpc_cidr" {
     type = string
 }
 
-variable "route53_zone" {
-    type = string
-}
-
 variable "env_eks_name" {
-    type = string
-}
-
-variable "service_name" {
-    type = string
-}
-
-variable "project_vpc_id" {
-    type = string
-}
-
-variable "project_vpc_private_subnets" {
     type = string
 }
