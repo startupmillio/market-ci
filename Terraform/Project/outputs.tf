e9dc9e284f863345a71f973f1bbf3a1e5af1c256
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-19 20:30:57 +0300

    subnets var with single quotes

--- Terraform/Project/outputs.tf
+++ Terraform/Project/outputs.tf
@@ -2,6 +2,10 @@ output "project_vpc_id"{
     value = module.vpc.vpc_id
 }
 
+locals {
+  vpc_private_subnets = format("['%s']", join("','", module.vpc.private_subnets))
+}
+
 output "project_vpc_private_subnets"{
-    value = module.vpc.private_subnets
+    value = local.vpc_private_subnets
 }
