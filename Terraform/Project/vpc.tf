e4733736d7ada2f515f03fd7d15a461d78c04fa9
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-06-20 12:53:11 +0300

    subnets as tf variables using cidrsubnet

--- Terraform/Project/vpc.tf
+++ Terraform/Project/vpc.tf
@@ -5,8 +5,8 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
   cidr                   = "${var.vpc_cidr}"
-  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets        = ["${cidrsubnet("${var.vpc_cidr}", 8, 1)}", "${cidrsubnet("${var.vpc_cidr}", 8, 2)}", "${cidrsubnet("${var.vpc_cidr}", 8, 3)}"] # Fix hardcoded values later!
+  public_subnets         = ["${cidrsubnet("${var.vpc_cidr}", 8, 101)}", "${cidrsubnet("${var.vpc_cidr}", 8, 102)}", "${cidrsubnet("${var.vpc_cidr}", 8, 103)}"] # Fix hardcoded values later!
 
   azs                    = data.aws_availability_zones.available.names
   enable_nat_gateway     = true
