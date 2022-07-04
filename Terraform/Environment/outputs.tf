b0dc385de57e0722cb73edfbe6ae9919241f28c9
Michael Ankudo <riuzaver@MacBook-Pro-Michael.local>
2022-07-04 16:16:54 +0300

    k8s values sensitive status

--- Terraform/Environment/outputs.tf
+++ Terraform/Environment/outputs.tf
@@ -1,15 +1,19 @@
 output "k8s_cluster_endpoint"{
     value = data.aws_eks_cluster.market-cluster.endpoint
+    sensitive   = true
 }
 
 output "k8_cluster_cert"{
     value = data.aws_eks_cluster.market-cluster.certificate_authority.0.data
+    sensitive   = true
 }
 
 output "k8s_cluster_name" {
     value = data.aws_eks_cluster.market-cluster.name
+    sensitive   = true
 }
 
 output "k8s_cluster_token" {
     value = data.aws_eks_cluster_auth.market-cluster.token
+    sensitive   = true
 }
